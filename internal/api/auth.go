cff93f1874d004fe66b27da619b29a6bc091df1b
Devil <81461889+nikitamirzani323@users.noreply.github.com>
2026-02-08 00:16:43 +0700

    auth limiter

--- internal/api/auth.go
+++ internal/api/auth.go
@@ -8,6 +8,7 @@ import (
 	"time"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/limiter"
 )
 
 type authApi struct {
@@ -18,7 +19,18 @@ func NewAuth(app *fiber.App, authService domain.AuthService) {
 	aa := &authApi{
 		authService: authService,
 	}
-	app.Post("/auth", aa.Login)
+	auth := app.Group("/auth", limiter.New(limiter.Config{
+		Max:        5,
+		Expiration: 1 * time.Minute,
+		KeyGenerator: func(c *fiber.Ctx) string {
+			return c.IP()
+		},
+		LimitReached: func(c *fiber.Ctx) error {
+			return c.Status(fiber.StatusTooManyRequests).
+				JSON(dto.CreateResponseError(fiber.StatusTooManyRequests, "too many requests"))
+		},
+	}))
+	auth.Post("", aa.Login)
 }
 func (a authApi) Login(ctx *fiber.Ctx) error {
 	c, cancel := context.WithTimeout(ctx.Context(), 10*time.Second)
