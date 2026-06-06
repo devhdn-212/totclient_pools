a319400b772e7a7ea34f800c122e79c41ac5a99f
Devil <81461889+nikitamirzani323@users.noreply.github.com>
2026-06-06 12:17:02 +0700

    update connection database

--- internal/connection/database.go
+++ internal/connection/database.go
@@ -13,29 +13,33 @@ import (
 )
 
 func GetDatabase(conf config.Database) *pgxpool.Pool {
-	dsn := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?search_path=%s&sslmode=disable&timezone=Asia/Jakarta",
-		conf.User,
-		conf.Pass,
-		conf.Host,
-		conf.Port,
-		conf.Name,
-		conf.Schema,
-	)
+	// 1. Buat config dasar pakai string minimalis tanpa password yang aneh-aneh
+	baseDSN := fmt.Sprintf("postgres://%s:%s/subapp?sslmode=disable", conf.Host, conf.Port)
 
-	// 2. Parsing konfigurasi pool
-	configPool, err := pgxpool.ParseConfig(dsn)
+	configPool, err := pgxpool.ParseConfig(baseDSN)
 	if err != nil {
 		Log.Fatal("Gagal parsing config database", zap.Error(err))
 	}
 
-	// 3. Pengaturan Connection Pool (Mirip dengan sql.DB)
-	configPool.MaxConns = 100                     // SetMaxOpenConns
-	configPool.MinConns = 10                      // SetMaxIdleConns (pendekatan terdekat)
-	configPool.MaxConnIdleTime = 5 * time.Minute  // SetConnMaxIdleTime
-	configPool.MaxConnLifetime = 60 * time.Minute // SetConnMaxLifetime
+	// 2. Isi data aslinya langsung ke objek struct-nya (Aman dari eror karakter @ atau !)
+	configPool.ConnConfig.User = conf.User
+	configPool.ConnConfig.Password = conf.Pass
+	configPool.ConnConfig.Database = conf.Name
+
+	// Jika ingin menambahkan runtime params seperti search_path atau timezone
+	if configPool.ConnConfig.RuntimeParams == nil {
+		configPool.ConnConfig.RuntimeParams = make(map[string]string)
+	}
+	configPool.ConnConfig.RuntimeParams["search_path"] = conf.Schema
+	configPool.ConnConfig.RuntimeParams["timezone"] = "Asia/Jakarta"
+
+	// 3. Pengaturan Connection Pool
+	configPool.MaxConns = 100
+	configPool.MinConns = 10
+	configPool.MaxConnIdleTime = 5 * time.Minute
+	configPool.MaxConnLifetime = 60 * time.Minute
 
 	// 4. Membuat koneksi pool
-	// Context background digunakan karena ini inisialisasi awal
 	dbPool, err := pgxpool.NewWithConfig(context.Background(), configPool)
 	if err != nil {
 		Log.Fatal("Gagal membuat pool database", zap.Error(err))
